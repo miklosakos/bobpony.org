eea6dd5ef72c0b10a446e52e769cbcb906b0c60f
TTtie <tttiett@outlook.com>
2018-08-04 16:26:10 +0200

    Compact this again

--- js/member-count.js
+++ js/member-count.js
@@ -1,9 +1,1 @@
-var R = new XMLHttpRequest();
-R.open("GET", "https://canary.discordapp.com/api/guilds/251862462343806976/widget.json", true);
-R.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        var Q = JSON.parse(this.responseText);
-        document.getElementById("memberCount").innerText = " There are currently " + Q.members.length + " members online.";
-    }
-};
-R.send();
+(function(){var a=new XMLHttpRequest();a.open("GET","https://discordapp.com/api/guilds/251862462343806976/widget.json",!0);a.onreadystatechange=function(){var b=this;if(b.readyState==4&&b.status==200){var c=JSON.parse(b.responseText);document.getElementById("memberCount").innerText=" There are currently "+c.members.length+" members online.";}};a.send();})()
